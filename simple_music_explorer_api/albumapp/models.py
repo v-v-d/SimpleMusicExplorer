7973a4de1e2bf0f763ae03fae213f72f5cefbaea
Kirill Gorshkoff <gobzter@gmail.com>
2020-06-29 15:20:19 +0300

    added authapp relations

--- simple_music_explorer_api/albumapp/models.py
+++ simple_music_explorer_api/albumapp/models.py
@@ -1,11 +1,11 @@
 from datetime import date
 
 from django.db import models
+from authapp.models import Artist
 
 
 class AlbumModel(models.Model):
     class Meta:
-        db_table = 'albums'
         constraints = [
             models.CheckConstraint(check=models.Q(price__gte=0), name='price_gte_0'),
         ]
@@ -15,22 +15,19 @@ class AlbumModel(models.Model):
     genre = models.CharField(max_length=32)
     date = models.DateField(default=date.today)
     description = models.CharField(max_length=512)
-    artist_name = models.CharField(max_length=128, null=False)
-    artist_id = models.IntegerField(null=False)
+    artist_id = models.ForeignKey('Artist', null=False, on_delete=models.CASCADE)
 
     # TODO: add a cover art
 
 
 class TrackModel(models.Model):
     class Meta:
-        db_table = 'tracks'
         unique_together = ['album', 'order']
         ordering = ['order']
 
-    artist_name = models.CharField(max_length=128, null=False)
-    artist_id = models.IntegerField(null=False)
+    artist_id = models.ForeignKey('Artist', null=False, on_delete=models.CASCADE)
     title = models.CharField(max_length=64, null=False)
-    album = models.ForeignKey('AlbumModel', related_name='tracks', null=False, on_delete=models.CASCADE)
+    album = models.ForeignKey('AlbumModel', related_name='tracks', null=False, on_delete=models.DO_NOTHING)
     order = models.IntegerField()
 
     def __str__(self):
