8802868c48a6de5f72d152ed10a60e6709d63da8
Kirill Gorshkoff <gobzter@gmail.com>
2020-06-30 17:57:04 +0300

    changed urls

--- simple_music_explorer_api/musicapp/urls.py
+++ simple_music_explorer_api/musicapp/urls.py
@@ -3,14 +3,14 @@ from django.urls import path
 from musicapp import views
 
 urlpatterns = [
-    path('', views.AlbumsListView.as_view(), name='albums'),
-    path('<int:pk>', views.AlbumDetailView.as_view(), name='album'),
-    path('<int:pk>/tracks', views.TrackListView.as_view(), name='tracks'),
+    path('albums', views.AlbumsListView.as_view(), name='albums'),
+    path('album/<int:pk>', views.AlbumDetailView.as_view(), name='album'),
+    path('album/<int:pk>/tracks', views.TrackListView.as_view(), name='tracks'),
 
-    path('artists/', views.ArtistListView.as_view(), name='artists'),
-    path('artist/', views.ArtistView.as_view(), name='artist'),
-    path('artist/<int:pk>/', views.ArtistDetail.as_view(), name='artist_detail'),
+    path('artists', views.ArtistListView.as_view(), name='artists'),
+    path('', views.ArtistView.as_view(), name='artist'),
+    path('<int:pk>/', views.ArtistDetail.as_view(), name='artist_detail'),
+    path('<int:pk>/albums', views.AlbumListView.as_view(), name='artist_albums'),
 
-    path('artist/<int:pk>/albums', views.AlbumListView.as_view(), name='artist_albums'),
     path('upload_cover', views.FileUploadView.as_view(), name='upload_cover')
 ]
