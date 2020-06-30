2023df804943b3093f526bba0db95f5f3a6303cf
Kirill Gorshkoff <gobzter@gmail.com>
2020-06-30 21:42:32 +0300

    new url hierarchy fixes

--- simple_music_explorer_api/musicapp/urls.py
+++ simple_music_explorer_api/musicapp/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from musicapp import views
 
 urlpatterns = [
-    path('albums', views.AlbumsListView.as_view(), name='albums'),
-    path('albums/<int:pk>', views.AlbumDetailView.as_view(), name='album'),
-    path('albums/<int:pk>/tracks', views.TrackListView.as_view(), name='tracks'),
+    path('albums/', views.AlbumsListView.as_view(), name='albums'),
+    path('albums/<int:pk>/', views.AlbumDetailView.as_view(), name='album'),
+    path('albums/<int:pk>/tracks/', views.TrackListView.as_view(), name='tracks'),
 
-    path('artists', views.ArtistListView.as_view(), name='artists_all'),
+    path('artists/', views.ArtistListView.as_view(), name='artists_all'),
     path('artists/<int:pk>/', views.ArtistDetail.as_view(), name='artist_detail'),
-    path('artists/<int:pk>/albums', views.AlbumListView.as_view(), name='artist_albums'),
+    path('artists/<int:pk>/albums/', views.AlbumListView.as_view(), name='artist_albums'),
 
     path('upload_cover', views.FileUploadView.as_view(), name='upload_cover')
 ]
