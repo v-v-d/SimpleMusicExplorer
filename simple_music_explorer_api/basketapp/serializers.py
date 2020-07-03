abd7d5f2d05fdbbdfd5b35ed7ca738acba8c3c8a
Kirill Gorshkoff <gobzter@gmail.com>
2020-07-03 16:36:34 +0300

    added small comment

--- simple_music_explorer_api/basketapp/serializers.py
+++ simple_music_explorer_api/basketapp/serializers.py
@@ -4,6 +4,11 @@ from basketapp.models import BasketModel
 
 
 class BasketSerializer(serializers.ModelSerializer):
+    """
+    A default class that can be used to represent the current user.
+    In order to use this, the 'request' must have been provided as part of the context dictionary
+    when instantiating the serializer.
+    """
     owner = serializers.CurrentUserDefault()
 
     class Meta:
