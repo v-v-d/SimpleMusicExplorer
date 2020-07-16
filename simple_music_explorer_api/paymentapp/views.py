8d9fc469be948b919ad61030469214f381603ff5
dagrishin <grishin-dim@yandex.ru>
2020-07-16 17:27:21 +0500

    update checkout orders

--- simple_music_explorer_api/paymentapp/views.py
+++ simple_music_explorer_api/paymentapp/views.py
@@ -8,10 +8,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from orderapp.models import Orders
+from orderapp.serializers import OrderSerializer
 from paymentapp.models import BillingInformation
 
 
 def checkout_orders(order, token):
+    serializers = OrderSerializer(order, many=True)
     headers = {
         "Content-Type": "application/json",
         "Authorization": 'Bearer' + token}
@@ -22,90 +24,20 @@ def checkout_orders(order, token):
         "application_context": {
             "return_url": settings.RETURN_URL,
             "cancel_url": settings.CANCEL_URL,
-            "brand_name": "EXAMPLE INC",
+            "brand_name": "SimpleMusicExplorer",
             "landing_page": "BILLING",
             "shipping_preference": "SET_PROVIDED_ADDRESS",
-            "user_action": "CONTINUE"
+            "user_action": "PAY_NOW"
         },
         "purchase_units": [
             {
-                "reference_id": "PUHF",
-                "description": "Sporting Goods",
-
-                "custom_id": "CUST-HighFashions",
-                "soft_descriptor": "HighFashions",
+                "soft_descriptor": "SimpleMusicExplorer",
                 "amount": {
                     "currency_code": "USD",
-                    "value": "220.00",
-                    "breakdown": {
-                        "item_total": {
-                            "currency_code": "USD",
-                            "value": "180.00"
-                        },
-                        "shipping": {
-                            "currency_code": "USD",
-                            "value": "20.00"
-                        },
-                        "handling": {
-                            "currency_code": "USD",
-                            "value": "10.00"
-                        },
-                        "tax_total": {
-                            "currency_code": "USD",
-                            "value": "20.00"
-                        },
-                        "shipping_discount": {
-                            "currency_code": "USD",
-                            "value": "10"
-                        }
-                    }
+                    "value": f"{order.total_sum}",
+
                 },
-                "items": [
-                    {
-                        "name": "T-Shirt",
-                        "description": "Green XL",
-                        "sku": "sku01",
-                        "unit_amount": {
-                            "currency_code": "USD",
-                            "value": "90.00"
-                        },
-                        "tax": {
-                            "currency_code": "USD",
-                            "value": "10.00"
-                        },
-                        "quantity": "1",
-                        "category": "PHYSICAL_GOODS"
-                    },
-                    {
-                        "name": "Shoes",
-                        "description": "Running, Size 10.5",
-                        "sku": "sku02",
-                        "unit_amount": {
-                            "currency_code": "USD",
-                            "value": "45.00"
-                        },
-                        "tax": {
-                            "currency_code": "USD",
-                            "value": "5.00"
-                        },
-                        "quantity": "2",
-                        "category": "PHYSICAL_GOODS"
-                    }
-                ],
-                "shipping": {
-                    "method": "United States Postal Service",
-                    "name": {
-                        "full_name": "John Doe"
-                    },
-                    "address": {
-                        "address_line_1": "123 Townsend St",
-                        "address_line_2": "Floor 6",
-                        "admin_area_2": "San Francisco",
-                        "admin_area_1": "CA",
-                        "postal_code": "94107",
-                        "country_code": "US"
-                    }
-                }
+                "items": serializers.data,
             }
         ]
 
