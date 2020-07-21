f72a421a9de5f748ad575ad9f0ae0b2fe3df3921
Kirill Gorshkoff <gobzter@gmail.com>
2020-07-21 10:43:14 +0300

    created basic user creation test to insure that djoser works

--- simple_music_explorer_api/authapp/tests.py
+++ simple_music_explorer_api/authapp/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from authapp.models import User
+from faker import Faker
+
+class AccountTests(APITestCase):
+    def test_create_account(self):
+        """
+        basic check to insure djoser works
+        """
+
+        fake = Faker()
+        url = reverse('user-list')
+
+        username = fake.first_name()
+        password = fake.password(length=15, special_chars=True)
+        email = fake.free_email()
+
+        data = {
+            'username': username,
+            'password': password,
+            're_password': password,
+            'email': email
+        }
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, username)
