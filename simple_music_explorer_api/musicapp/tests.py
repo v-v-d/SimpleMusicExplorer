a092d001695013ca94ffd8577ab631e26d1366e1
Kirill Gorshkoff <gobzter@gmail.com>
2020-07-23 15:23:01 +0300

    added retrieve test

--- simple_music_explorer_api/musicapp/tests.py
+++ simple_music_explorer_api/musicapp/tests.py
@@ -1,12 +1,15 @@
-from django.urls import reverse
-from rest_framework import status
-from simple_music_explorer_api.test_core import TestCore
+import json
 
+from django.urls import reverse
 from faker import Faker
+from rest_framework import status
+
+from simple_music_explorer_api.test_core import TestCore
 
 
 class Artist(TestCore):
-    def test_create_artist(self):
+    def test_create_retrieve_artist(self):
+        # creating fake artist
         fake = Faker()
         user = self.create_and_auth_user()
 
@@ -22,6 +25,13 @@ class Artist(TestCore):
 
         request = self.client.post(url, data, format='json')
         self.assertEqual(request.status_code, status.HTTP_201_CREATED)
+
+        # retrieving user artist
+        request = self.client.get(url)
+        result = json.loads(request.content.decode())[0]
+
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(data['user'], result['user']['id'])
 
 
 class GetAlbums(TestCore):
