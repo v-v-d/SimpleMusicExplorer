2814d8e08e4881a67fb5849eb7d8a2df9b43aa3b
v-v-d <v-v-d@bk.ru>
2020-08-14 02:43:29 +0300

    Merchapp models modified

--- simple_music_explorer_api/merchapp/models.py
+++ simple_music_explorer_api/merchapp/models.py
@@ -1,30 +1,38 @@
 from django.db import models
 
-# Create your models here.
-from coreapp.models import Core
-from musicapp.models import ArtistModel, FileModel
+from coreapp.models import Core, FileModel
+from musicapp.models import Artist
 
 
 class ProductCategory(Core):
     class Meta:
-        verbose_name = 'Категория продукта'
-        verbose_name_plural = 'Категории продуктов'
+        verbose_name = 'Product category'
+        verbose_name_plural = 'Product categories'
 
     def __str__(self):
         return self.title
 
 
+class ProductManager(models.QuerySet):
+    def delete(self, **kwargs):
+        for obj in self:
+            obj.is_active = False
+            obj.save()
+
+
 class Product(Core):
     class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
 
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
-    artist = models.ForeignKey(ArtistModel, on_delete=models.CASCADE)
-    image = models.ManyToManyField(FileModel)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    image = models.ManyToManyField(FileModel, blank=True)
     short_desc = models.CharField(verbose_name='Product short description', max_length=60, blank=True)
     price = models.DecimalField(verbose_name='Product price', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name='Product quantity in stock', default=0)
+
+    objects = ProductManager.as_manager()
 
     def __str__(self):
         return f'{self.title} ({self.category.title})'
