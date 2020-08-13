3cb0bf8c363a043954990eabfe527028da1cced2
v-v-d <v-v-d@bk.ru>
2020-08-14 02:50:37 +0300

    Debug route for static retrieving added

--- simple_music_explorer_api/simple_music_explorer_api/urls.py
+++ simple_music_explorer_api/simple_music_explorer_api/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.schemas import get_schema_view
@@ -35,3 +37,6 @@ urlpatterns = [
     path('api/v1/seller/', include('sellerapp.urls'), name='seller'),
     path('api/v1/customer/', include('customerapp.urls'), name='customer'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
