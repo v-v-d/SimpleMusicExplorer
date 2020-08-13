41bd74facf579a0a250f01d0baea207f25af511e
v-v-d <v-v-d@bk.ru>
2020-08-14 02:42:28 +0300

    file model moved to coreapp models

--- simple_music_explorer_api/coreapp/models.py
+++ simple_music_explorer_api/coreapp/models.py
@@ -1,16 +1,17 @@
+from pathlib import Path
+
 from django.db import models
 
 
 class Core(models.Model):
-
     title = models.CharField(max_length=100, blank=True)
     description = models.TextField(blank=True)
-    active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
     date = models.DateTimeField(auto_now_add=True, blank=False)
     date_modif = models.DateTimeField(auto_now=True, blank=True)
 
     def __str__(self):
-        return f'{self.title}' if self.title else ''
+        return f'{self.title}' if self.title else super().__str__()
 
     # def delete(self, **kwargs):
     #     if 'force' in kwargs:
@@ -20,6 +21,15 @@ class Core(models.Model):
     #         self.save()
 
 
-class Article(Core):
-    pass
+def file_path(instance, filename):
+    # file will be uploaded to the path like
+    # media/albums/<filename.ext>
+    return Path(filename)
 
+
+class FileModel(models.Model):
+    file = models.FileField(blank=False, null=False, upload_to=file_path)
+    default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.file.name
